b35ae6c26c467753ffc186d045da4b57ae3062df
Laddge <dev.laddge@gmail.com>
2023-10-12 14:28:12 +0000

    create layout

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,12 @@
 </script>
 
 <template>
+  <header class="navbar bg-base-100">
+    <a class="btn btn-ghost normal-case text-xl">共通テスト配点計算ツール</a>
+  </header>
+  <main class="p-4 max-w-xl mx-auto">
+  </main>
+  <footer class="sticky top-[100vh] text-center text-sm p-4" style="top: 100dvh;">
+    &copy; 2023 <a href="https://laddge.net" target="_blank" class="link">Laddge</a>
+  </footer>
 </template>
